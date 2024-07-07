46c13aa8e333be8d5c7a1ff8f035a0f4e1c40baa
caaaan <canborcbakan@gmail.com>
2024-07-07 11:59:28 +0200

    deleted some comments

--- audioplayer.py
+++ audioplayer.py
@@ -9,30 +9,24 @@ class AudioPlayer():
 
     def play(self, audiopath):
         self._running = True
-        #storing how much we have read already
         self.chunktotal = 0
         wf = wave.open(audiopath, 'rb')
         stream = self.audio.open(format =self.audio.get_format_from_width(wf.getsampwidth()),channels = wf.getnchannels(),rate = wf.getframerate(),output = True)
         print(wf.getframerate())
-        # read data (based on the chunk size)
         data = wf.readframes(self.chunk)
-        #THIS IS THE TOTAL LENGTH OF THE AUDIO
         audiolength = wf.getnframes() / float(wf.getframerate())
 
         while self._running:
             if data != '':
                 stream.write(data)
                 self.chunktotal = self.chunktotal + self.chunk
-                #calculating the percentage
                 percentage = (self.chunktotal/wf.getnframes())*100
-                #calculating the current seconds
                 current_seconds = self.chunktotal/float(wf.getframerate())
                 data = wf.readframes(self.chunk)
 
             if data == b'':
                 break
 
-        # cleanup stream
         stream.close()
 
     def stop(self):
